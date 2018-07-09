2a35ed6f0094d64b24e04488b9ad1a5674d1370c
Arthur Allamand <a.allamand@gmail.com>
2018-07-09 19:55:24 +0200

    test js

--- app/javascript/components/menu.js
+++ app/javascript/components/menu.js
@@ -2,6 +2,7 @@ function menuToogle() {
   const menu = document.getElementsByClassName('hamburger--minus');
   menu[0].addEventListener("click", (event) => {
     menu[0].classList.toggle("is-active");
+    console.log('hey');
   });
 }
 export {menuToogle};
