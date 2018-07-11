9b687db9d3c40fb3446ef3afc7908efe5af8c100
Arthur Allamand <a.allamand@gmail.com>
2018-07-11 18:20:24 +0200

    contact and links

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   mount ForestLiana::Engine => '/forest'
   devise_for :users
   root to: 'pages#home'
+  get "/pages/contact" => "pages#contact"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
