4a45eaa3348541a93ee385f09c43fe5c74e0a815
Arthur Allamand <a.allamand@gmail.com>
2018-07-18 11:01:18 +0200

    tried to add postmark

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,12 @@
 class Message < ApplicationRecord
+  validates :email, presence: true, format: { with: /@/ }
+  validates :name, presence: true
+  validates :subject, presence: true
+  validates :description, presence: true
+  after_create :send_welcome_email
+
+  private
+  def send_welcome_email
+    MessageMailer.welcome(self.id).deliver_later
+  end
 end
